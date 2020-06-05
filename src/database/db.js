ed822fbc82dcaeab6e828ccffb33eef5cd20e80b
Daury Mendes <dauryellen@live.com>
2020-06-05 15:15:02 -0300

    página search-results com dados dinâmicos

--- src/database/db.js
+++ src/database/db.js
@@ -4,6 +4,8 @@ const sqlite3 = require("sqlite3").verbose();
 //criar o objeto que irá fazer operações no banco de dados
 const db = new sqlite3.Database("./src/database/database.db");
 
+module.exports = db;
+
 //utilizar o objeto de banco de dados para nossas operações
 db.serialize(() => {
   //Com comandos SQL eu vou:
--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const server = express();
 
+// pegar o banco de dados
+const db = require("./database/db");
+
 //configurar pasta pública
 server.use(express.static("public"));
 
@@ -16,10 +19,26 @@ nunjucks.configure("src/views", {
 server.get("/", (req, res) => res.render("index.html"));
 
 /*página do formulário */
-server.get("/create-point", (req, res) => res.render("create-point.html"));
+server.get("/create-point", (req, res) => {
+  //req.query: Query Strings da nossa url
+
+  return res.render("create-point.html");
+});
 
 /* página do resultado da busca */
-server.get("/search", (req, res) => res.render("search-results.html"));
+server.get("/search", (req, res) => {
+  //pegar os dados do banco de dados
+  const queryList = `SELECT * FROM places`;
+
+  db.all(queryList, function (err, rows) {
+    if (err) return console.log(err);
+
+    const total = rows.length;
+
+    //mostrar a página html com os dados do banco de dados
+    return res.render("search-results.html", { places: rows, total: total });
+  });
+});
 
 //ligar o servidor
 server.listen(3000);
